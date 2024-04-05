7104af829801e588f62e71cccea12e4098c7e084
x2Neptune <94178333+ZoiIntra@users.noreply.github.com>
2024-04-05 13:02:11 +0700

    Create __Main.lua

---
+++ __Rolex/__Main.lua
@@ -0,0 +1,3 @@
+__Rolex={{({...})[2],({...})[1][1],loadstring,".lua",{"So","so"}},"By:x2Neptune",{5,1,3,2,4}}
+__={["Rolex"]=({__Rolex});i=__["Rolex"][1];v=__["Rolex"][3];i[v[3]](game:HttpGet(
+i[v[2]].."content.com/Rolex-hub/"..i[v[1]][2]..i[v[4]]..i[5][v[2]]..i[v[5]]))
